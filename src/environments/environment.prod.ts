a945cde7b24bb676e2e93827617101436976a40c
MainStreetCode <114355456+MainStreetCode@users.noreply.github.com>
2024-12-03 15:49:38 -0500

    Develop (#7)
    * add dist folder for deployment to github pages
    
    * try to get gh pages to work
    
    * remove extra files
    
    * move files torneo folder
    
    * move everything back to docs folder
    
    * change base href
    
    * Add npm script to build to docs for gh pages
    
    * build using prod config
    
    ---------
    
    Co-authored-by: Ron Fessler <ron.fessler@tylertech.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://mainstreetcode.github.io/torneo/',
+  firebase: {
+    apiKey: "AIzaSyBXfstBvichb-xN3QAYd4Xx_03-j3FMaxA",
+    authDomain: "tourney-84ede.firebaseapp.com",
+    projectId: "tourney-84ede",
+    storageBucket: "tourney-84ede.appspot.com",
+    messagingSenderId: "720024685972",
+    appId: "1:720024685972:web:27d438c5628bc965749381",
+    measurementId: "G-DJDJLLFS6V"
+  }
 };
